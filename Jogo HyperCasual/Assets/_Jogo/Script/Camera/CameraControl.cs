77e3196071bb922261d9a649ab0622b4387fa15d
Anthony Cruz <88720778+OppaiTaon@users.noreply.github.com>
2021-10-11 23:56:43 -0500

    Background repeat
    Com algumas complicacoes ainda mas ta praticamente pegando

--- Jogo HyperCasual/Assets/_Jogo/Script/Camera/CameraControl.cs
+++ Jogo HyperCasual/Assets/_Jogo/Script/Camera/CameraControl.cs
@@ -5,16 +5,45 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     public Transform target;
+
+    public Transform bg1;
+    public Transform bg2;
+
+    private float size;
+
+
     void Start()
     {
-        
+        size = bg1.GetComponent<BoxCollider2D>().size.y;
     }
 
     
     void FixedUpdate() 
     {
+        //camera
         Vector3 targetPos = new Vector3(target.position.x, target.position.y, transform.position.z);
 
         transform.position = Vector3.Lerp(transform.position, targetPos, 0.2f);
+    
+        //background
+        if(transform.position.y >= bg2.position.y)
+        {
+            bg1.position = new Vector3(bg1.position.x, bg2.position.y + size, bg1.position.z);
+            SwitchBg();
+        }
+
+        if(transform.position.y< bg1.position.y)
+        {
+            bg2.position = new Vector3(bg2.position.x, bg1.position.y - size, bg2.position.z);
+            SwitchBg();
+        }
     }
+
+    private void SwitchBg()
+    {
+        Transform temp = bg1;
+        bg1 = bg2;
+        bg2 = temp;
+    }
+
 }
